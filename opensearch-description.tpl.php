651db335abc067d1b6018db126b501ec32ac2897
Alberto Paderno <kiam.git@avpnet.org>
2010-09-26 14:40:45 +0000

    By kiam: Updated HEAD.

--- opensearch-description.tpl.php
+++ opensearch-description.tpl.php
@@ -1,5 +1,35 @@
-<?php /* $Id$ */ ?>
-<?xml version="1.0" encoding="UTF-8"?>
+<?php
+// $Id$
+
+/**
+ * @file
+ * Displays a Opensearch description file.
+ *
+ * Available variables:
+ * - $shortname: The short name.
+ * - $description: The description.
+ * - $contact: The contact email address for the site.
+ * - $url_opensearch: The URL for the Opensearch feed.
+ * - $url_self: The URL for the Opensearch description file.
+ * - $tags: A set of words that are used as keywords to identify and categorize
+ *   this search content. Keywords must be a single word and are delimited by
+ *   the space character.
+ * - $longname: An extended human-readable title that identifies this search
+ *   engine.
+ * - $attribution: A list of all sources or entities that should be credited
+ *   for the content contained in the search feed.
+ * - $syndication_right: The degree to which the search results provided by
+ *   this search engine can be queried, displayed, and redistributed.
+ * - $adult_content: A boolean value that is set to TRUE if the search results
+ *   may contain material intended only for adults.
+ * - $image_attributes: A string containing the image attributes for the image
+ *   to use for the search engine.
+ * - $image_url: The URL for the image to use for the search engine.
+ * - $languages: An array that indicates for which languages the search engine
+ *   returns search results.
+ */
+?>
+<?php print "<?xml"; ?> version="1.0" encoding="UTF-8" <?php print "?>"; ?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
   <ShortName><?php print $shortname; ?></ShortName>
   <Description><?php print $description; ?></Description>
